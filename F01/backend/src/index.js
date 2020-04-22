2a01506b26dc1688863e07466b5531f55103c1c9
Franccesco Antonio Ferrarezi Bordon <franccesco_@hotmail.com>
2020-04-21 21:28:23 -0300

    F01: Primeira fase concluída
    Durante a primeira fase da nova jornada, relembrei alguns conceitos de Javascript em NodeJS, ReactJS e React Native e tivemos uma pequena introdução ao Typescript

---
+++ F01/backend/src/index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const cors = require('cors');
+const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar informações do back-end;
+ * POST: Criar uma informação no back-end;
+ * PUT/PATCH: Alterar uma informação no back-end;
+ * DELETE: Deletar uma informação no back-end;
+ *
+ */
+
+/**
+ * Tipos de parâmetros
+ *
+ * Query Params: Filtros e Paginação
+ * Route Params: Identificar recursos (atualizaer/deletar)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+ *
+ */
+
+/**
+ *
+ * Middleware:
+ *
+ * Interceptador de requisições que pode interromper totalmente a requisição ou alterar dados da requisição
+ *
+ */
+
+const projects = [];
+
+function logReq(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); // Próximo middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validadeProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid Project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logReq);
+app.use('projects/:id', validadeProjectId);
+
+app.get('/projects', (req, res) => {
+  const { title } = req.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return res.json(results);
+});
+
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, owner } = req.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
+});
+
+app.listen(3333, () => {
+  console.log('Back-end Started!');
+});
