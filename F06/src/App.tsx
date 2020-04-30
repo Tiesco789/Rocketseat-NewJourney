ab82fb654d711b4f7eda0068241e3e70be02f01c
Tiesco789 <franccesco_@hotmail.com>
2020-04-30 20:42:06 -0300

    Startign GoBarber Layout

---
+++ F06/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import SignIn from './pages/Signin/index';
+import SignUp from './pages/Signup/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
